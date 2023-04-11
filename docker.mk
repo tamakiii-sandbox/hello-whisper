ee64543d4cd8c4f1588feaa6e07ea6cfe08259de
tamakiii <tamakiii@users.noreply.github.com>
2023-04-11 17:06:40 +0900

    add bash rule

--- docker.mk
+++ docker.mk
@@ -1,4 +1,4 @@
-.PHONY: help
+.PHONY: help build bash clean
 
 IMAGE := tamakiii-sandbox/hello-whisper
 LABEL := latest
@@ -8,6 +8,9 @@ help:
 
 build: Dockerfile
 	docker build -t $(IMAGE):$(LABEL) .
+
+bash:
+	docker run -it --rm -v $(PWD):/usr/local/lib/tamakiii-sandbox/hello-whisper -w /usr/local/lib/tamakiii-sandbox/hello-whisper $(IMAGE):$(LABEL) $@
 
 clean:
 	docker image rm $(IMAGE):$(LABEL)
